1297b7dce5a680ad002b62166d0cc08861b1ea1d
Ajela13 <alejamarino123@gmail.com>
2025-05-01 22:21:23 -0500

    feat: add portfolio layout adn style

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,7 +1,20 @@
 import "./Portfolio.css";
+import ProjectList from "../ProjectList/ProjectList";
+import { projects } from "../../data/projects";
 
 function Portfolio() {
-  return <section className="portfolio" id="portfolio"></section>;
+  return (
+    <section className="portfolio" id="portfolio">
+      <h2 className="portfolio__title">PORTFOLIO</h2>
+      <div className="portfolio__content">
+        <ul className="portfolio__list">
+          {projects.map((item) => {
+            return <ProjectList key={item.id} item={item} />;
+          })}
+        </ul>
+      </div>
+    </section>
+  );
 }
 
 export default Portfolio;
