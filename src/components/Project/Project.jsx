f7a5449f150fbb91bbb371d4bd1ba9ed124d275b
Ajela13 <alejamarino123@gmail.com>
2025-05-01 22:25:00 -0500

    feat: create project layout and style

---
+++ src/components/Project/Project.jsx
@@ -0,0 +1,23 @@
+import "./Project.css";
+
+function Project({ item }) {
+  return (
+    <div className="project">
+      <div className="project__item-description">
+        <h3 className="project__item-title">{item.name}</h3>
+        <p className="project__item-text">{item.description}</p>
+        <p className="project__item-tech">{item.technologies}</p>
+        <a href="#" className="project__item-link">
+          <p className="project__item-link-text">Learn more</p>
+        </a>
+      </div>
+      <img
+        src={item.image}
+        alt="Project 1"
+        className="project__item-image"
+      ></img>
+    </div>
+  );
+}
+
+export default Project;
