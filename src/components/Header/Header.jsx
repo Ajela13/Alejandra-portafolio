8ac185db295942a098ab134baa241c084fa2f49e
Alejamarino <amarino@visualcontact.com.co>
2025-09-05 17:46:38 -0500

    Feat: add resume button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,6 +3,42 @@ import "./Header.css";
 function Header() {
   return (
     <header className="header" id="header">
+      {/* Download Resume Icon/Link in top right */}
+      <div
+        style={{
+          position: "absolute",
+          top: "20px",
+          right: "30px",
+          zIndex: 10,
+          display: "flex",
+          alignItems: "center",
+          gap: "8px",
+        }}
+      >
+        <a
+          href="/resume.pdf"
+          download
+          title="Download Resume"
+          style={{
+            color: "#222",
+            textDecoration: "none",
+            display: "flex",
+            alignItems: "center",
+            gap: "6px",
+          }}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+          >
+            <path d="M10 13.586l4.293-4.293 1.414 1.414L10 16.414l-5.707-5.707 1.414-1.414L10 13.586zM4 4h12v2H4V4zm0 4h12v2H4V8z" />
+          </svg>
+          <span style={{ fontSize: "14px" }}>Download Resume</span>
+        </a>
+      </div>
       <div className="header__container">
         <div className="header__intro">
           <p className="header__intro-description">welcome to the</p>
