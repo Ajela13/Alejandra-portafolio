73b01c4fc00632fc13613b33f20f67e59c52ec47
Alejamarino <amarino@visualcontact.com.co>
2025-09-05 20:09:14 -0500

    Fix: fix guthub pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Alejandra-portafolio/', // 👈 IMPORTANT: must match your repo name
 })
